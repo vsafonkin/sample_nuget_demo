990f5876f8c33c3de20ab4f8981ecc28f6a93236
Vladimir Safonkin <vsafonkin@github.com>
2021-03-09 12:47:13 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_nuget_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
